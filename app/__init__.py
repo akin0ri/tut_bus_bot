69dff06e33ce9ddba694bf72631f9fb2bc0e5028
akin0ri <akinori.u.2003.jp3@gmail.com>
2024-02-18 16:19:06 +0000

    update line bot sdk v2 to v3

--- app/__init__.py
+++ app/__init__.py
@@ -1,40 +1,53 @@
 from os import environ
 from dotenv import load_dotenv
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
 
 app = Flask(__name__)
 load_dotenv(".env", verbose=True)
 
+# set LINE channel secret and access token
 if not (access_token := environ.get("LINE_CHANNEL_ACCESS_TOKEN")):
     raise Exception("access token is not set as an environment variable")
-
 if not (channel_secret := environ.get("LINE_CHANNEL_SECRET")):
     raise Exception("channel secret is not set as an environment variable")
 
-# Line Messaging APIのチャネルアクセストークンとチャネルシークレットを設定してください
-line_bot_api = LineBotApi(access_token)
+line_bot_api = Configuration(access_token)
 handler = WebhookHandler(channel_secret)
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+
+    # get request body as text
     body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
+
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
+
+@handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    message = event.message.text
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=message)
-    )
+    with ApiClient(Configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=3131, debug=True)
