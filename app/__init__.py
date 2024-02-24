96cef27071d5736b820efd6286a0adbfd0e57f40
akin0ri <akinori.u.2003.jp3@gmail.com>
2024-02-24 09:16:09 +0000

    add time table func

--- app/__init__.py
+++ app/__init__.py
@@ -44,12 +44,13 @@ def callback():
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
-        print(event.message.text)
+        bustype, direction = event.message.text.split("_")
+        timetable = get_last_5_bus_times(bustype, int(direction))
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
+                messages=[TextMessage(text=timetable)]
             )
         )
 
---
+++ app/test.py
@@ -0,0 +1,3 @@
+from bus_time import get_last_5_bus_times
+
+print(get_last_5_bus_times("hachioji", 1))
