708129fe577ed573c7374aae35b1fb9adc3f8b14
akin0ri <akinori.u.2003.jp3@gmail.com>
2024-09-12 07:11:34 +0000

    fix バス運行情報

--- app/bus_status.py
+++ app/bus_status.py
@@ -23,71 +23,37 @@ def get_bus_status(num):
     reply_text_list = f"【運行予定 {text_date.strftime('%H:%M:%S')}現在】\n\n"
     for i in range(num):
         now_date = datetime.now(timezone(timedelta(hours=+9), 'JST')) + timedelta(days=int(i))
-        
+        # now_date = datetime(2024, 9, 29, 12, 30, 0, 0, tzinfo=timezone(timedelta(hours=+9), 'JST'))
+        isWeekdays = now_date.weekday() < 5
+
         # extraordinary setting start
         # extraordinary = 1 に該当する日付を設定
         extraordinary_1_dates = [
-            datetime(2024, 7, 27, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 7, 28, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 3, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 4, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 17, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 18, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 19, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 24, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 25, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 9, 1, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
             datetime(2024, 9, 16, tzinfo=timezone(timedelta(hours=+9), 'JST')).date()
         ]
 
-        # extraordinary = 2 の条件: 2024年8月7日〜9月10日（除外日を除く）かつ月〜金曜日のみ
-        extraordinary_2_dates = [
-            datetime(2024, 8, 7, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 8, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 9, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 19, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 20, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 26, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 27, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 28, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 8, 29, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 9, 2, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 9, 3, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 9, 4, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 9, 5, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 9, 6, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 9, 9, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 9, 10, tzinfo=timezone(timedelta(hours=+9), 'JST')).date()
-        ]
-
-        # extraordinary = 3 の条件: 2024年8月31日・9月7日
-        extraordinary_3_dates = [
-            datetime(2024, 8, 31, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(),
-            datetime(2024, 9, 7, tzinfo=timezone(timedelta(hours=+9), 'JST')).date()
-        ]
-
-        # extraordinary = 4 の条件: 2024年8月10日～18日、8月21日～25日
-        extraordinary_4_periods = [
-            (datetime(2024, 8, 10, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(), datetime(2024, 8, 18, tzinfo=timezone(timedelta(hours=+9), 'JST')).date()),
-            (datetime(2024, 8, 21, tzinfo=timezone(timedelta(hours=+9), 'JST')).date(), datetime(2024, 8, 25, tzinfo=timezone(timedelta(hours=+9), 'JST')).date())
-        ]
-        print(now_date)
+        # extraordinary = 2 の条件: 2024年9月11日(水) ～ 9月21日(土)
+        extraordinary_2_start = datetime(2024, 9, 11, tzinfo=timezone(timedelta(hours=+9), 'JST')).date()
+        extraordinary_2_end = datetime(2024, 9, 21, tzinfo=timezone(timedelta(hours=+9), 'JST')).date()
 
         # extraordinary の値を設定
-        if now_date.date() in extraordinary_3_dates:
-            text = f"{now_date.month}月{now_date.day}日({get_weekday_jst(now_date)})：臨時(土曜)での運行"
-        elif now_date.date() in extraordinary_1_dates:
-            text = f"{now_date.month}月{now_date.day}日({get_weekday_jst(now_date)})：臨時(オーキャンなど)での運行"
-        elif now_date.date() in extraordinary_2_dates:
-            text = f"{now_date.month}月{now_date.day}日({get_weekday_jst(now_date)})：臨時(平日)での運行"
-        elif any(start <= now_date.date() <= end for start, end in extraordinary_4_periods):
-            text = f"{now_date.month}月{now_date.day}日({get_weekday_jst(now_date)})：運行なし"
-        else:
-            if now_date.weekday() == 6:
+        if now_date.date() in extraordinary_1_dates:
+            text = f"{now_date.month}月{now_date.day}日({get_weekday_jst(now_date)})：臨時(オーキャン等)ダイヤ"
+        elif extraordinary_2_start <= now_date.date() <= extraordinary_2_end:
+            if isWeekdays:
+                text = f"{now_date.month}月{now_date.day}日({get_weekday_jst(now_date)})：長期休暇(平日)ダイヤ"
+            elif now_date.weekday() == 6:
                 text = f"{now_date.month}月{now_date.day}日({get_weekday_jst(now_date)})：運行なし"
             else:
-                text = f"{now_date.month}月{now_date.day}日({get_weekday_jst(now_date)})：通常での運行"            
-            
+                text = f"{now_date.month}月{now_date.day}日({get_weekday_jst(now_date)})：長期休暇(土曜)ダイヤ"
+        else:
+            if isWeekdays:
+                text = f"{now_date.month}月{now_date.day}日({get_weekday_jst(now_date)})：通常(平日)ダイヤ"
+            elif now_date.weekday() == 6:
+                text = f"{now_date.month}月{now_date.day}日({get_weekday_jst(now_date)})：運行なし"
+            else:
+                text = f"{now_date.month}月{now_date.day}日({get_weekday_jst(now_date)})：通常(土曜)ダイヤ"
+
         reply_text_list += text + "\n"
 
     reply_text_list += "\n詳しいバス運行情報は公式サイトをご確認ください。(https://www.teu.ac.jp/campus/access/006644.html)"
